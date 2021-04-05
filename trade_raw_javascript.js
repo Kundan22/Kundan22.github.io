97a733318c0195e69cf30b3c50974906287f58fb
Kundan22 <macpro1001@gmail.com>
2021-04-05 14:41:13 +0000

    deploy: Kundan22/ScriptJunction@c5775c3bf8903e048c18872c4b5b182516ad6be1

--- trade_raw_javascript.js
+++ trade_raw_javascript.js
@@ -23,7 +23,7 @@ function CalculateIntraday(controlID) {
     GST = (brokerage + tranCharges) * 0.18
     stampDuty = turnOver * 0.000015
  
-    totalCharges = brokerage + STT + tranCharges + sebiCharges + GST + stampDuty;
+    totalCharges = parseFloat(brokerage) + Math.round(parseFloat(STT)) + parseFloat(tranCharges) + parseFloat(sebiCharges) + parseFloat(GST) + parseFloat(stampDuty);
     document.getElementById("Brokerage"+rowNumber).value = totalCharges.toFixed(2)
  
     breakEven = ((buyPrice * purchaseQty) + totalCharges)/purchaseQty
@@ -31,7 +31,7 @@ function CalculateIntraday(controlID) {
  }
  
  function CalculateDelivery(controlID) {
-    debugger;
+  debugger;
     rowNumber = getRowNumber(controlID);
     document.getElementById("Buy"+rowNumber).value = document.getElementById("Buy"+rowNumber).value.replace(",","")
  
@@ -42,24 +42,28 @@ function CalculateIntraday(controlID) {
  
     target = buyPrice + (buyPrice * (targetPercent/100))
     sellPrice = target
-    profit = (target - buyPrice) * purchaseQty
+    profit = (target.toFixed(2) - buyPrice.toFixed(2)) * purchaseQty
  
     turnOver = (buyPrice + sellPrice) * purchaseQty
+    turnOver = turnOver.toFixed(2)
     brokerage = 0
-    STT = turnOver * 0.001
-    tranCharges = turnOver * 0.0000345
-    sebiCharges = turnOver * 0.0000005
-    GST = (brokerage + tranCharges) * 0.18
-    stampDuty = buyPrice * purchaseQty * 0.00015
+    STT = parseFloat(turnOver * 0.001)
+    tranCharges = parseFloat(turnOver * 0.0000345)
+    sebiCharges = parseFloat(turnOver * 0.0000005)
+    GST = parseFloat((brokerage + tranCharges) * 0.18)
+    stampDuty = parseFloat(buyPrice * purchaseQty * 0.00015)
  
-    totalCharges = brokerage + STT + tranCharges + sebiCharges + GST + stampDuty;
-    document.getElementById("Brokerage"+rowNumber).value = totalCharges.toFixed(2)
- 
-    breakEven = ((buyPrice * purchaseQty) + totalCharges)/purchaseQty
-    document.getElementById("Breakeven"+rowNumber).value = breakEven.toFixed(2)
- 
-    document.getElementById("Target"+rowNumber).value = target.toFixed(2);
-    document.getElementById("Profit"+rowNumber).value = (profit - totalCharges).toFixed(2);
+    var totalCharges = parseFloat(brokerage) + Math.round(parseFloat(STT)) + parseFloat(tranCharges) + parseFloat(sebiCharges) + parseFloat(GST) + parseFloat(stampDuty);
+    
+    
+    //  alert(totalCharges); 
+      document.getElementById("Brokerage"+rowNumber).value = totalCharges.toFixed(2)
+  
+      breakEven = ((buyPrice * purchaseQty) + totalCharges)/purchaseQty
+      document.getElementById("Breakeven"+rowNumber).value = breakEven.toFixed(2)
+  
+      document.getElementById("Target"+rowNumber).value = target.toFixed(2);
+      document.getElementById("Profit"+rowNumber).value = (profit - totalCharges).toFixed(2);
     
  }
  
